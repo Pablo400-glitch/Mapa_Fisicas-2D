c7fc5b440b51a12dc5e2f8c0d0e97529937437f9
Pablo400-glitch <paisficretpablo@gmail.com>
2024-10-21 20:44:48 +0100

    Added to gitattributes

---
+++ Mapa-Físicas_FDV_/Assets/Scripts/ImpulseZone.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0108f937f9bf3f3710f893c8e038a1a11c2c0fbdd8c1dd0dd3e52f4467f3b636
+size 414
---
+++ Mapa-Físicas_FDV_/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5dd7ed1e0151aa86371479ebaa9714d92dbc145b4f06ee2eafde9c2690739bc2
+size 2574
